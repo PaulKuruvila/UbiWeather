1f265b8a85952eebbf74cc9b8871464b2d622a7f
PaulKuruvila <paul.paulkuruvila@gmail.com>
2022-04-11 01:05:03 -0500

    Backend integration
    Implementing express.js for backend

---
+++ frontend_react/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+import Navbar from './components/Navbar/Navbar';
+import Display from './components/Display/Display';
+
+function App() {
+
+  const cityName = "Sugar Land";
+
+  return (
+    <div className="App">
+      <Navbar />
+      <div className="main">
+        <Display city={cityName}/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
